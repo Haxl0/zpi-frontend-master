623737869d889832b30523fd358208147adbba8d
HaxHex <damian9611@gmail.com>
2021-11-30 18:33:01 +0100

    test

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
 
-Vue.config.productionTip = false
+import Vue from 'vue';
+import DashboardPlugin from './plugins/dashboard-plugin';
+import App from './App.vue';
 
+import router from './routes/router';
+
+Vue.use(DashboardPlugin);
+
+/* eslint-disable no-new */
 new Vue({
+  el: '#app',
   render: h => h(App),
-}).$mount('#app')
+  router
+});
