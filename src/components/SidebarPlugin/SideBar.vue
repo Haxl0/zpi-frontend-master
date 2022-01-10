1e964d8df7097e3031f89ca6b0a590a7cf0e5cb0
HaxHex <damian9611@gmail.com>
2022-01-10 18:02:21 +0100

    Delete logo, sidebar, Add new title - Instalacja Odsiarczania spalin

--- src/components/SidebarPlugin/SideBar.vue
+++ src/components/SidebarPlugin/SideBar.vue
@@ -6,74 +6,9 @@
             <navbar-toggle-button @click.native="showSidebar">
 
             </navbar-toggle-button>
-            <router-link class="navbar-brand" to="/">
-                <img :src="logo" class="navbar-brand-img" alt="...">
-            </router-link>
 
-            <slot name="mobile-right">
-                <ul class="nav align-items-center d-md-none">
-                    <base-dropdown class="nav-item" menu-on-right tag="li" title-tag="a">
-                        <a slot="title-container" class="nav-link nav-link-icon" href="#" role="button"
-                           aria-haspopup="true" aria-expanded="false">
-                            <i class="ni ni-bell-55"></i>
-                        </a>
-
-                        <a class="dropdown-item" href="#">Action</a>
-                        <a class="dropdown-item" href="#">Another action</a>
-                        <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#">Something else here</a>
-                    </base-dropdown>
-                    <base-dropdown class="nav-item" menu-on-right tag="li" title-tag="a">
-                        <a slot="title-container" class="nav-link" href="#" role="button">
-                            <div class="media align-items-center">
-                              <span class="avatar avatar-sm rounded-circle">
-                                <img alt="Image placeholder" src="img/theme/team-1.jpg">
-                              </span>
-                            </div>
-                        </a>
-
-                        <div class=" dropdown-header noti-title">
-                            <h6 class="text-overflow m-0">Welcome!</h6>
-                        </div>
-                        <router-link to="/profile" class="dropdown-item">
-                            <i class="ni ni-single-02"></i>
-                            <span>My profile</span>
-                        </router-link>
-                        <router-link to="/profile" class="dropdown-item">
-                            <i class="ni ni-settings-gear-65"></i>
-                            <span>Settings</span>
-                        </router-link>
-                        <router-link to="/profile" class="dropdown-item">
-                            <i class="ni ni-calendar-grid-58"></i>
-                            <span>Activity</span>
-                        </router-link>
-                        <router-link to="/profile" class="dropdown-item">
-                            <i class="ni ni-support-16"></i>
-                            <span>Support</span>
-                        </router-link>
-                        <div class="dropdown-divider"></div>
-                        <a href="#!" class="dropdown-item">
-                            <i class="ni ni-user-run"></i>
-                            <span>Logout</span>
-                        </a>
-                    </base-dropdown>
-                </ul>
-            </slot>
-            <slot></slot>
             <div v-show="$sidebar.showSidebar" class="navbar-collapse collapse show" id="sidenav-collapse-main">
 
-                <div class="navbar-collapse-header d-md-none">
-                    <div class="row">
-                        <div class="col-6 collapse-brand">
-                            <router-link to="/">
-                                <img :src="logo">
-                            </router-link>
-                        </div>
-                        <div class="col-6 collapse-close">
-                            <navbar-toggle-button @click.native="closeSidebar"></navbar-toggle-button>
-                        </div>
-                    </div>
-                </div>
 
                 <ul class="navbar-nav">
                     <slot name="links">
@@ -87,19 +22,19 @@
                 <ul class="navbar-nav mb-md-3">
                     <li class="nav-item">
                         <a class="nav-link"
-                           href="https://www.creative-tim.com/learning-lab/bootstrap-vue/alerts/argon-dashboard">
+                           href="https://id.atlassian.com/login?continue=https%3A%2F%2Fzpicyber.atlassian.net%2Flogin%3FredirectCount%3D1%26dest-url%3D%252Fjira%252Fsoftware%252Fprojects%252FZPI%252Fboards%252F1%26application%3Djira&application=jira">
                             <i class="ni ni-spaceship"></i> JIRA
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link"
-                           href="https://www.creative-tim.com/learning-lab/bootstrap-vue/colors/argon-dashboard">
+                           href="https://github.com/Haxl0/zpi-frontend-master">
                             <i class="ni ni-palette"></i> GITHUB Frontend
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link"
-                           href="https://www.creative-tim.com/learning-lab/bootstrap-vue/alerts/argon-dashboard">
+                           href="https://github.com/mdembiczak/zpi-backend">
                             <i class="ni ni-ui-04"></i> GITHUB Backend
                         </a>
                     </li>
@@ -117,11 +52,6 @@
       NavbarToggleButton
     },
     props: {
-      logo: {
-        type: String,
-        default: 'img/brand/green.png',
-        description: 'Sidebar app logo'
-      },
       autoClose: {
         type: Boolean,
         default: true,
