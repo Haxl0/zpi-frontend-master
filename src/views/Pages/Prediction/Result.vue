385f131c8f13484d54a127db01d58ce79b7b57fc
HaxHex <damian9611@gmail.com>
2022-01-14 19:59:13 +0100

    Change result field color

--- src/views/Pages/Prediction/Result.vue
+++ src/views/Pages/Prediction/Result.vue
@@ -1,44 +1,24 @@
 <template>
-  <b-card-body v-if="result.value < 200" class="pt-2 d-flex justify-content-center align-items-center h-100 text-center bg-success">
+  <b-card-body class="pt-2 d-flex justify-content-center align-items-center h-100 text-center ">
     <div>
       <b-row>
         <b-col lg="12" class="p-0">
           <h1>Wynik:</h1>
           <br>
-          <h2>{{ result.value }}</h2>
+          <h2  v-if="result.value < 200" class="text-success">{{ result.value }}</h2>
+          <h2  v-else-if="result.value >=200 && result.value < 400"  class="text-warning">{{ result.value }}</h2>
+          <h2  v-else class="text-danger">{{ result.value }}</h2>
         </b-col>
       </b-row>
     </div>
   </b-card-body>
-      <b-card-body v-else-if="result.value >=200 && result.value < 400" class="pt-2 d-flex justify-content-center align-items-center h-100 text-center bg-warning ">
-        <div>
-          <b-row>
-            <b-col lg="12" class="p-0">
-              <h1>Wynik:</h1>
-              <br>
-              <h2>{{ result.value }}</h2>
-            </b-col>
-          </b-row>
-        </div>
-      </b-card-body>
-    <b-card-body v-else class="pt-2 d-flex justify-content-center align-items-center h-100 text-center bg-danger">
-      <div>
-        <b-row>
-          <b-col lg="12" class="p-0">
-            <h1>Wynik:</h1>
-            <br>
-            <h2>{{ result.value }}</h2>
-          </b-col>
-        </b-row>
-      </div>
-    </b-card-body>
 </template>
 
 <script>
-  export default {
-    name: "Result",
-    props: {
-      result: Object
-    }
- }
+export default {
+  name: "Result",
+  props: {
+    result: Object
+  }
+}
 </script>
